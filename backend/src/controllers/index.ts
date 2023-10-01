7b107699693d2c72178b654584525562d2b72ecd
mfcastilho <fredericocastilho@hotmail.com>
2023-10-01 13:31:41 -0300

    feat: insert CreateUserController at the index file

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,6 +1,7 @@
-
+import CreateUserController from "./CreateUserController";
 import GetAllCategoriesController from "./GetAllCategoriesController";
 
 export {
+     CreateUserController,
      GetAllCategoriesController
 };
