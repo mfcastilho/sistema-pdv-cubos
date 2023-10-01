7de6edf4763048ecb0716b729ac33fd9e3213ecc
mfcastilho <fredericocastilho@hotmail.com>
2023-10-01 20:00:30 -0300

    feat: create LoginUserService

---
+++ backend/src/services/LoginUserService.ts
@@ -0,0 +1,40 @@
+import { UserRepository } from "../repositories";
+import jwt from "jsonwebtoken";
+
+
+interface LoginUserDTO {
+     id:string,
+     name:string,
+     email:string,
+     password:string
+}
+
+class LoginUserService {
+     async execute({ email }:LoginUserDTO) {
+          
+          try {
+               const repo = UserRepository;
+
+               const user:LoginUserDTO | null = await repo.findFirst({
+                    where: {email}
+               });
+
+               if(user) {
+                    const jwtSecretkey = process.env.JWT_SECRET_KEY; 
+
+                    if(!jwtSecretkey) return new Error("A chave secreta JWT não está definida.");
+
+                    const token = jwt.sign({id:user.id}, jwtSecretkey, {expiresIn:"24h"});
+
+                    const { password, ...userLogged } = user;
+
+                    return {user: userLogged, token};                   
+               }
+
+          } catch (error) {
+               throw error;
+          }
+     }
+}
+
+export default LoginUserService;
