7f27338f5fdb4df9ba417ab4e3c7166d87a05816
mfcastilho <fredericocastilho@hotmail.com>
2023-10-01 02:46:31 -0300

    feat: create user and category models

---
+++ backend/prisma/migrations/20231001054248_create_categories_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
