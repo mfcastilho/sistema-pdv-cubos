84bc4629a2810ece628f90d92a94f7c8c3082d5b
mfcastilho <fredericocastilho@hotmail.com>
2023-10-02 00:00:34 -0300

    update: remove unnecessary return message at the verifyLoginUserFields middleware

--- backend/src/middlewares/verifyLoginUserFields.ts
+++ backend/src/middlewares/verifyLoginUserFields.ts
@@ -8,25 +8,14 @@ const verifyLoginUserFields = async (req: Request, res: Response, next: NextFunc
 
                email: Joi.string()
                     .trim()
-                    .email({ tlds: { allow: false } })
                     .required()
                     .messages({
-                         'any.required': "O campo email é obrigatório.",
-                         'string.base': "Formato de email inválido.",
-                         'string.email': "Formato de email inválido.",
                          'string.empty': "Todos os campos são obrigatórios."
                     }),
 
                password: Joi.string()
                     .trim()
-                    .min(5)
-                    .max(16)
-                    .pattern(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@#$%^&+=!*]).{8,}$/)
                     .messages({
-                         'any.required': "O campo senha é obrigatório.",
-                         'string.min': "A senha deve conter no mínimo 5 e no máximo 16 caracteres.", 
-                         'string.max': "A senha deve conter no mínimo 5 e no máximo 16 caracteres.", 
-                         'string.pattern.base': "A senha deve conter letras, números e caracteres especiais.",
                          'string.empty': "Todos os campos são obrigatórios."
                     })     
      });
