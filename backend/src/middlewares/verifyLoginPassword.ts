60ee02b52abfe92ac516fcf8fdc14eedcf06a6d0
mfcastilho <fredericocastilho@hotmail.com>
2023-10-01 23:39:32 -0300

    feat: create verifyLoginPassword Middleware

---
+++ backend/src/middlewares/verifyLoginPassword.ts
@@ -0,0 +1,32 @@
+import { UserRepository } from "../repositories";
+import { Request, Response, NextFunction } from 'express';
+import bcrypt from "bcrypt";
+
+
+const verifyLoginPassword = async (req: Request, res: Response, next: NextFunction)=>{
+     try {
+
+          const { email, password } = req.body;
+
+          const repo = UserRepository;
+
+          const user = await repo.findFirst({
+               where:{email}
+          });
+
+          const cryptedPassword = user?.password;
+
+          if(!cryptedPassword) return res.status(500).json({error: "Senha não encontrada."});
+
+          const passwordIsValid = bcrypt.compare(password, cryptedPassword);
+
+          if(!passwordIsValid) return res.status(401).json({error: "Email e/ou senha incorreto."});
+
+          return next();
+          
+     } catch (error) {
+          return res.status(500).json({error: "Erro interno do servidor"});    
+     }
+}
+
+export default verifyLoginPassword;
