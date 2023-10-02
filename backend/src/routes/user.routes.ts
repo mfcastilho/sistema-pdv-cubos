52f7be489f963004fc4d56f0d8e0bbfccd1c3255
mfcastilho <fredericocastilho@hotmail.com>
2023-10-01 23:45:43 -0300

    feat: create LoginUser route

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { CreateUserController } from "../controllers";
-import { verifyCreateUserFields, verifyIfEmailExists } from "../middlewares";
+import { CreateUserController, LoginUserController } from "../controllers";
+
+import {  verifyCreateUserFields, 
+          verifyIfEmailExists,
+          verifyLoginEmail,
+          verifyLoginPassword, } from "../middlewares";
 
 const userRoutes = Router();
 
 userRoutes.post("/usuario", verifyCreateUserFields, verifyIfEmailExists, CreateUserController.handle);
+
+userRoutes.post("/login", verifyLoginEmail, verifyLoginPassword, LoginUserController.handle);
 
 
 export default userRoutes;
