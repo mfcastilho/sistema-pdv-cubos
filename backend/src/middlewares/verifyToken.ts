20f928bfb37098daf172fa3058dd862a590b2369
mfcastilho <fredericocastilho@hotmail.com>
2023-10-02 00:26:41 -0300

    feat: create verifyToken midlleware

--- backend/src/middlewares/verifyToken.ts
+++ backend/src/middlewares/verifyToken.ts
@@ -1,6 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from "jsonwebtoken";
 
+declare global {
+     namespace Express {
+          interface Request {
+               decoded?: any;
+          }
+     }
+}
 
 const verifyToken = (req: Request, res: Response, next: NextFunction)=>{
 
@@ -16,6 +23,10 @@ const verifyToken = (req: Request, res: Response, next: NextFunction)=>{
           if(!jwtSecretKey) return res.status(500).json({error: "A chave secreta JWT não está definida."});
 
           const decoded = jwt.verify(token, jwtSecretKey);
+
+          req.decoded = decoded;
+
+          return next();
           
      } catch (error) {
           return res.status(500).json({error: "Para acessar este recurso um token de autenticação válido deve ser enviado."});
