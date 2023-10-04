ee8f07ab3d2178d4e24ecb1531fbf7c31dfb291a
mfcastilho <fredericocastilho@hotmail.com>
2023-10-04 08:32:00 -0300

    feat: insert product.routes and client.routes at the index file

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,12 @@
 import categoryRoutes from "./category.routes";
+import clientRoutes from "./client.routes";
+import productRoutes from "./product.routes";
 import userRoutes from "./user.routes";
 
 
 export {
      userRoutes,
-     categoryRoutes
+     categoryRoutes,
+     productRoutes,
+     clientRoutes
 }
