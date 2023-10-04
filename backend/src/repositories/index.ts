a1b2accc0d2d5c1c79b47af4eb8e7ec9d4286dc9
mfcastilho <fredericocastilho@hotmail.com>
2023-10-04 08:36:58 -0300

    feat: insert product.repository and client.repository at the index file

--- backend/src/repositories/index.ts
+++ backend/src/repositories/index.ts
@@ -1,8 +1,12 @@
 import CategoryRepository from "./category.repository";
 import UserRepository from "./user.repostitory";
+import ProductRepository from "./product.repository";
+import ClientRepository from "./client.repository";
 
 
 export {
      UserRepository,
-     CategoryRepository
+     CategoryRepository,
+     ClientRepository,
+     ProductRepository
 };
