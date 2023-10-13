e687e4931b12a9e30d55bc02e1ffed6ebe265d9a
mfcastilho <fredericocastilho@hotmail.com>
2023-10-13 02:32:36 -0300

    feat: create get all products route

--- backend/src/routes/product.routes.ts
+++ backend/src/routes/product.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
-import { RegisterProductController, EditProductController } from "../controllers";
+import { RegisterProductController, 
+         EditProductController, 
+         GetAllProductsController } from "../controllers";
 
-import {  verifyToken, 
-          verifyProductFields, 
-          verifyIfCategoryExists } from "../middlewares";
+import { verifyToken, 
+         verifyProductFields, 
+         verifyIfCategoryExists } from "../middlewares";
 
 const productRoutes = Router();
 
@@ -12,6 +14,8 @@ const productRoutes = Router();
 productRoutes.post("/produto", verifyToken, verifyProductFields, verifyIfCategoryExists, RegisterProductController.handle);
 
 productRoutes.put("/produto/:id", verifyToken, verifyProductFields, verifyIfCategoryExists, EditProductController.handle);
+
+productRoutes.get("/produto", verifyToken, GetAllProductsController.handle);
 
 
 export default productRoutes;
