6854a74a007fea9a47c60a3598d7789d337679cb
mfcastilho <fredericocastilho@hotmail.com>
2023-10-13 01:56:10 -0300

    feat: register product and edit product routes

--- backend/src/routes/product.routes.ts
+++ backend/src/routes/product.routes.ts
@@ -1,5 +1,17 @@
 import { Router } from "express";
 
+import { RegisterProductController, EditProductController } from "../controllers";
+
+import {  verifyToken, 
+          verifyProductFields, 
+          verifyIfCategoryExists } from "../middlewares";
+
 const productRoutes = Router();
+
+
+productRoutes.post("/produto", verifyToken, verifyProductFields, verifyIfCategoryExists, RegisterProductController.handle);
+
+productRoutes.put("/produto/:id", verifyToken, verifyProductFields, verifyIfCategoryExists, EditProductController.handle);
+
 
 export default productRoutes;
