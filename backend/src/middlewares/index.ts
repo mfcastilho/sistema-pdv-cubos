bba16a4de4cf02c8c2469b43a1eda65465132ac5
mfcastilho <fredericocastilho@hotmail.com>
2023-10-13 01:53:42 -0300

    feat: insert verifyProductFields middleware at the index file

--- backend/src/middlewares/index.ts
+++ backend/src/middlewares/index.ts
@@ -1,8 +1,11 @@
 import verifyCreateUserFields from "./verifyCreateUserFields";
+import verifyEditUserFields from "./verifyEditUserFields";
+import verifyIfCategoryExists from "./verifyIfCategoryExists";
 import verifyIfEmailExists from "./verifyIfEmailExists";
 import verifyLoginEmail from "./verifyLoginEmail";
 import verifyLoginPassword from "./verifyLoginPassword";
 import verifyLoginUserFields from "./verifyLoginUserFields";
+import verifyProductFields from "./verifyProductFields";
 import verifyToken from "./verifyToken";
 
 
@@ -13,5 +16,8 @@ export {
      verifyLoginEmail,
      verifyLoginPassword,
      verifyLoginUserFields,
-     verifyToken
+     verifyToken,
+     verifyIfCategoryExists,
+     verifyProductFields,
+     verifyEditUserFields
 };
