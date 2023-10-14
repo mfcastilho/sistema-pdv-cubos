e0ac2ed6088d4855c36a2a571acf91f379320f62
mfcastilho <fredericocastilho@hotmail.com>
2023-10-14 02:56:58 -0300

    feat: create CreateClient route

--- backend/src/routes/client.routes.ts
+++ backend/src/routes/client.routes.ts
@@ -1,5 +1,17 @@
 import { Router } from "express";
 
+import { CreateClientController } from "../controllers";
+
+import { validateRequestBody,
+         verifyToken,
+         verifyIfEmailExists,
+         verifyIfCpfExists } from "../middlewares";
+
+import { createClientSchema } from "../schemas"
+
 const clientRoutes = Router();
+
+
+clientRoutes.post("/cliente", verifyToken, validateRequestBody(createClientSchema), verifyIfEmailExists, verifyIfCpfExists, CreateClientController.handle);
 
 export default clientRoutes;
