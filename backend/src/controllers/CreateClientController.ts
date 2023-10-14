7910b7a3efdc4c83dfd2eccc3f54520d2cd313b7
mfcastilho <fredericocastilho@hotmail.com>
2023-10-14 00:44:29 -0300

    feat: create CreateClientController

---
+++ backend/src/controllers/CreateClientController.ts
@@ -0,0 +1,27 @@
+import { CreateClientService } from "../services";
+import { Request, Response } from "express";
+
+class CreateClientController {
+     async handle(req:Request, res:Response) {
+
+          try {
+
+               const { name, cpf, email } = req.body;
+
+               const  { zipCode, street, number, district, city, state } = req.body;
+
+               const service = new CreateClientService();
+
+               const clientCreated = await service.execute({ name, cpf, email }, { zipCode, street, number, district, city, state });
+
+               return res.status(201).json(clientCreated);
+               
+          } catch (error) {
+               console.log(error);
+               
+               return res.status(500).json({error: "Erro interno do servidor"});
+          }
+     }
+}
+
+export default new CreateClientController;
