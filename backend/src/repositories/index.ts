c403b6ed45ec74bd1d7d3abff0b432e3b7db9c45
mfcastilho <fredericocastilho@hotmail.com>
2023-10-13 21:27:27 -0300

    feat: insert AddressRepository at the index file

--- backend/src/repositories/index.ts
+++ backend/src/repositories/index.ts
@@ -2,11 +2,14 @@ import CategoryRepository from "./category.repository";
 import UserRepository from "./user.repostitory";
 import ProductRepository from "./product.repository";
 import ClientRepository from "./client.repository";
+import AddressRepository from "./address.repository";
+
 
 
 export {
      UserRepository,
      CategoryRepository,
      ClientRepository,
-     ProductRepository
+     ProductRepository,
+     AddressRepository
 };
