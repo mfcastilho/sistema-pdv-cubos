8aa776962a5daf56db4e06d80e98988dddc63cc7
mfcastilho <fredericocastilho@hotmail.com>
2023-10-14 22:16:27 -0300

    update: update validateRequestBody middleware

--- backend/src/middlewares/validateRequestBody.ts
+++ backend/src/middlewares/validateRequestBody.ts
@@ -1,52 +1,53 @@
 import { Request, Response, NextFunction } from 'express';
-import Joi from 'joi';
+import { ObjectSchema } from 'joi';
 
-const validateRequestBody = (joiSchema: Joi.ObjectSchema) => async (req: Request, res: Response, next: NextFunction)=>{
+const validateRequestBody = (joiSchema: ObjectSchema) => async (req: Request, res: Response, next: NextFunction)=>{
 
      try {
           
-          if(req.url === "/cliente") {
+        if(req.baseUrl === "/cliente") {
 
-               const { name, cpf, email } = req.body;
+            const { name, email, cpf } = req.body;
 
-               const  { zipCode, street, number, district, city, state } = req.body;
-   
-               if(req.addressRegistered) {
-                   await joiSchema.validateAsync(req.body);
-                   return next();
-               }
-   
-               if((zipCode && (!street || !number || !district || !city || !state)) ||
-               (street && (!zipCode || !number || !district || !city || !state)) ||
-               (number && (!street || !zipCode || !district || !city || !state)) ||
-               (district && (!street || !number || !zipCode || !city || !state)) ||
-               (city && (!street || !number || !district || !zipCode || !state)) ||
-               (state && (!street || !number || !district || !city || !zipCode))) {
-   
-                   return res.status(400).json({mensagem: "Se optar por cadastrar o endereço é obrigatório o preenchimento de todos os campos de endereço"});
-               }
-   
-               if(zipCode && street && number && district && city && state) {
+            const  { zipCode, street, number, district, city, state } = req.body;
 
-                   await joiSchema.validateAsync(req.body);
+            if(req.addressRegistered) {
+                await joiSchema.validateAsync(req.body);
+                return next();
+            }
 
-                   return next();
-   
-               }else{
+            if((zipCode && (!street || !number || !district || !city || !state)) ||
+            (street && (!zipCode || !number || !district || !city || !state)) ||
+            (number && (!street || !zipCode || !district || !city || !state)) ||
+            (district && (!street || !number || !zipCode || !city || !state)) ||
+            (city && (!street || !number || !district || !zipCode || !state)) ||
+            (state && (!street || !number || !district || !city || !zipCode))) {
 
-                   await joiSchema.validateAsync({ name, cpf, email });
+                return res.status(400).json({mensagem: "Se optar por cadastrar o endereço é obrigatório o preenchimento de todos os campos de endereço"});
+            }
 
-                   return next();
-               }
-           }
+            if(zipCode && street && number && district && city && state) {
+
+                await joiSchema.validateAsync(req.body);
+
+                return next();
+
+            }else{
+                
+                await joiSchema.validateAsync({ name, email, cpf });
+
+                return next();
+            }
+        }
            
            await joiSchema.validateAsync(req.body);
-
            return next();
 
      } catch (error: any) {
           return res.status(400).json({ mensagem: error.message });
      }
+
+     
 }
 
 export default validateRequestBody;
