07fbefe7508403871063056230f5d15ffc17230f
mfcastilho <fredericocastilho@hotmail.com>
2023-10-14 22:18:27 -0300

    update: change the route verification from req.utl to req.baseUrl at the verifyIfRmailExists middleware

--- backend/src/middlewares/verifyIfEmailExists.ts
+++ backend/src/middlewares/verifyIfEmailExists.ts
@@ -1,4 +1,4 @@
-import { UserRepository } from "../repositories";
+import { UserRepository, ClientRepository } from "../repositories";
 import { Request, Response, NextFunction } from 'express';
 
 const verifyIfEmailExists = async (req: Request, res: Response, next: NextFunction)=>{
@@ -6,26 +6,47 @@ const verifyIfEmailExists = async (req: Request, res: Response, next: NextFuncti
      try {
 
           const { email } = req.body;
-     
+
+          const userRepo = UserRepository;
+          const clientRepo = ClientRepository;
+
+          let emailExists;
+       
           if(email) {
-               const repo = UserRepository;
-     
-               const emailExists = await repo.findFirst({
-                    where: {
-                         email:email
-                    }
-               });
+
+               if(req.baseUrl === "/usuario"){
+                    emailExists = await userRepo.findFirst({
+                         where: {email}
+                    })
+                }
+                
+                if(req.baseUrl === "/cliente"){
+                    emailExists = await clientRepo.findFirst({
+                         where: {email}
+                    })
+                }
+               
           
                if(req.decoded){
                     
                     const userLoggedId = req.decoded.id;
           
-                    const userLogged = await repo.findUnique({
+                    const userLogged = await userRepo.findUnique({
                          where:{id:userLoggedId}
                     });
                
                     
-                    if(userLogged?.email === email) return next();    
+                    if(userLogged?.email === email) return next();
+                    
+                    
+                    if(req.params.id) {
+
+                         const client = await clientRepo.findFirst({
+                              where: {id: req.params.id}
+                         });
+     
+                         if(client?.email === email) return next();
+                    }
                }
           
                if(emailExists) return res.status(400).json({error: "Email já está cadastrado."});
