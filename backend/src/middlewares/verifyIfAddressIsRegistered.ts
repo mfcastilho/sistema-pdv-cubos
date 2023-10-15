3954827e3d987ff226f2a0f4797cdc7aa34dc20c
mfcastilho <fredericocastilho@hotmail.com>
2023-10-14 22:17:28 -0300

    update: insert try/catch at the verifyIfAddressIsRegistered middleware

--- backend/src/middlewares/verifyIfAddressIsRegistered.ts
+++ backend/src/middlewares/verifyIfAddressIsRegistered.ts
@@ -12,22 +12,26 @@ declare global {
 
 const verifyIfAddressIsRegistered = async (req: Request, res: Response, next: NextFunction)=>{
 
-     const repo = AddressRepository;
-
-     const  address = await repo.findFirst({
-          where: {clientId: req.params.id}
-     });
-
-     if(address) {
-          const { zipCode, street, number, district, city, state } = address;
-
-          if(zipCode === "N/A" && street === "N/A" && number === "N/A" && district === "N/A" && city === "N/A" && state === "XX") {
-               req.addressRegistered = false;
-           }else {
-               req.addressRegistered = true;
-           }
-   
-           return next();
+     try {
+          const repo = AddressRepository;
+     
+          const  address = await repo.findFirst({
+               where: {clientId: req.params.id}
+          });
+     
+          if(address) {
+               const { zipCode, street, number, district, city, state } = address;
+     
+               if(zipCode === "N/A" && street === "N/A" && number === "N/A" && district === "N/A" && city === "N/A" && state === "XX") {
+                    req.addressRegistered = false;
+                }else {
+                    req.addressRegistered = true;
+                }
+        
+                return next();
+          }
+     } catch (error) {
+          return res.status(500).json({ mensagem: "Erro interno do servidor"});
      }
 }
 
