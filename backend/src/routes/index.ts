58b8740412c49cf2affd1bd21a249bff5e70e0d5
mfcastilho <fredericocastilho@hotmail.com>
2023-10-14 22:18:59 -0300

    feat; insert login route at the index file

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,11 +1,13 @@
 import categoryRoutes from "./category.routes";
 import clientRoutes from "./client.routes";
+import loginRoutes from "./login.routes";
 import productRoutes from "./product.routes";
 import userRoutes from "./user.routes";
 
 
 export {
      userRoutes,
+     loginRoutes,
      categoryRoutes,
      productRoutes,
      clientRoutes
