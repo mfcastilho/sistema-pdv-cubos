ea1d5f3c9cab81ce09fe5ff8b0aed4de683090c2
mfcastilho <fredericocastilho@hotmail.com>
2023-11-08 03:03:43 -0300

    feat: create CreateOrderController

---
+++ backend/src/controllers/CreateOrderController.ts
@@ -0,0 +1,26 @@
+import { CreateOrderService } from "../services";
+import { Request, Response } from "express";
+
+
+class CreateOrderController {
+     async handle(req:Request, res:Response) {
+
+          try {
+
+               const { clientId, observation, orderProducts } = req.body;
+               
+               const service = new CreateOrderService();
+
+               const orderCreated = await service.execute({ clientId, observation, orderProducts });
+
+               return res.status(200).json(orderCreated);
+               
+          } catch (error) {
+               console.log(error);
+               
+               return res.status(500).json({error: "Erro interno do servidor"});
+          }
+     }
+}
+
+export default new CreateOrderController;
