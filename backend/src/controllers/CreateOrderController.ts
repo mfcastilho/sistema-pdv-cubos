a431ae670204ad71c8ba129149c65d61e596003f
mfcastilho <fredericocastilho@hotmail.com>
2023-11-08 17:36:41 -0300

    feat: create CreateOrderController

--- backend/src/controllers/CreateOrderController.ts
+++ backend/src/controllers/CreateOrderController.ts
@@ -15,9 +15,7 @@ class CreateOrderController {
 
                return res.status(200).json(orderCreated);
                
-          } catch (error) {
-               console.log(error);
-               
+          } catch (error) { 
                return res.status(500).json({error: "Erro interno do servidor"});
           }
      }
