6646930b71625b12347809cd64eb4e88daa3116e
mfcastilho <fredericocastilho@hotmail.com>
2023-11-08 17:34:49 -0300

    feat: insert all utils functions at the index file

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -2,10 +2,12 @@ import convertCentsToCurrent from "./convertCentsToCurrent";
 import convertCurrentToCents from "./convertCurrentToCents";
 import calculateTotalValue from "./calculateTotalValue";
 import getOrderProductsValues from "./getOrderProductsValues";
+import sendEmail from "./sendEmail";
 
 export {
      convertCentsToCurrent,
      convertCurrentToCents,
      calculateTotalValue,
-     getOrderProductsValues
+     getOrderProductsValues,
+     sendEmail
 }
