5e7220d3209e92034009a399aabe361402c2343e
mfcastilho <fredericocastilho@hotmail.com>
2024-01-27 03:49:20 -0300

    feat: insert the new functions at the index file from middlewares folde

--- backend/src/routes/product.routes.ts
+++ backend/src/routes/product.routes.ts
@@ -1,27 +1,30 @@
 import { Router } from "express";
+import multer from "../config/multer";
 
 import { RegisterProductController, 
          EditProductController, 
          GetAllProductsController,
          GetProductController,
-         DeleteProductController } from "../controllers";
+         DeleteProductController } from "../controllers/product";
 
 import { verifyToken, 
          verifyProductFields, 
          verifyIfCategoryExists,
-         verifyIfProductExists } from "../middlewares";
+         verifyIfProductExists,
+         verifyProductDeletion } from "../middlewares";
 
 const productRoutes = Router();
 
 
-productRoutes.post("/", verifyToken, verifyProductFields, verifyIfCategoryExists, RegisterProductController.handle);
+productRoutes.post("/", verifyToken, multer.single("productImage"), verifyProductFields, verifyIfCategoryExists, RegisterProductController.handle);
 
-productRoutes.put("/:id", verifyToken, verifyProductFields, verifyIfCategoryExists, EditProductController.handle);
+productRoutes.put("/:id", verifyToken, verifyIfProductExists, multer.single("productImage"), verifyProductFields, verifyIfCategoryExists, EditProductController.handle);
 
 productRoutes.get("/", verifyToken, GetAllProductsController.handle);
 
 productRoutes.get("/:id", verifyToken, verifyIfProductExists, GetProductController.handle);
 
-productRoutes.delete("/:id", verifyToken, verifyIfProductExists, DeleteProductController.handle);
+productRoutes.delete("/:id", verifyToken, verifyIfProductExists, verifyProductDeletion, DeleteProductController.handle);
+
 
 export default productRoutes;
