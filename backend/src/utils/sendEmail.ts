11147d9d928a0e0a27e59899d37c4f5aaabd1132
mfcastilho <fredericocastilho@hotmail.com>
2024-01-27 03:47:55 -0300

    feat: insert htmlCompiler file at the sendEmail file

--- backend/src/utils/sendEmail.ts
+++ backend/src/utils/sendEmail.ts
@@ -1,4 +1,6 @@
-import { Client } from './../entities';
+import { EmailOrderConfirmationDTO } from './../dto';
+
+import { htmlCompilator } from "../utils";
 
 const sgMail = require('@sendgrid/mail');
 
@@ -6,7 +8,10 @@ require("dotenv").config();
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const sendEmail = async ({name, email}: Client) => {
+
+const sendEmail = async ({name, email, orderNumber, observation, totalValue, products}: EmailOrderConfirmationDTO) => {
+
+     const html = await htmlCompilator({name, email, orderNumber, observation, totalValue, products});
 
      const msg = {
           to: [email],
@@ -14,19 +19,22 @@ const sendEmail = async ({name, email}: Client) => {
                name: "SomosDev Tecnologia",
                email: process.env.FROM_EMAIL
           }, 
-          templateId: process.env.TEMPLATE_ID,
-          dynamicTemplateData: {
-               name: name
-          }
+          subject: 'Confirmação do Pedido',
+          text: 'Sucesso na realização do Pedido!',
+          html,
+                    
+          // templateId: process.env.TEMPLATE_ID,
+          // dynamicTemplateData: {
+          //      name: name
+          // }
      };
 
      try {
           await sgMail.send(msg);
      } catch (error: any) {
           console.error(error);
-     
           if (error.response) {
-          console.error(error.response.body)
+               console.error(error.response.body)
           }
      }
 }
